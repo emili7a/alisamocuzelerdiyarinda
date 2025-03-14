855f316cffb4a45fbc62ba09e34719ecf8df1b7a
emilia <emiliyaaz-bb104@code.edu.az>
2025-03-14 04:01:27 +0400

    task1

---
+++ task1/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+string username = "mario";
+string password = "mario123";
+if (username == "admin" && password == "admin123")
+{
+    Console.WriteLine("xos gelmisiniz");
+}
+else
+{
+    Console.WriteLine("username ve ya password yalnisdir");
+}
+
