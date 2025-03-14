1a076cc29f92d26975b54eb37f22111602340eb8
emilia <emiliyaaz-bb104@code.edu.az>
2025-03-14 04:21:37 +0400

    task2

--- task1/task1/Program.cs
+++ task1/task1/Program.cs
@@ -18,30 +18,51 @@ Console.WriteLine("Hello, World!");
 
 
 
-int score = 65;
-if (score > 91 && score < 100)
+//int score = 65;
+//if (score > 91 && score < 100)
+//{
+//    Console.WriteLine("A");
+//}
+//else if (score > 80 && score < 90)
+//{
+//    Console.WriteLine("B");
+//}
+//else if (score > 70 && score < 79)
+//{
+//    Console.WriteLine("c");
+//}
+//else if (score > 60 && score < 69)
+//{
+//    Console.WriteLine("D");
+//}
+//else if (score > 51 && score < 59)
+//{
+//    Console.WriteLine("E");
+//}
+//else
+//{
+//    Console.WriteLine("Bdu telebesidirse kesilir");
+//}
+
+
+
+
+int number = 5;
+if (number > 10 || number < 99)
 {
-    Console.WriteLine("A");
+    Console.WriteLine(" 10 ve 99 arasinda ikireqemli ededi yazin");
 }
-else if (score > 80 && score < 90)
+int tens = number / 10;
+int units = number % 10;
+if (tens > units)
 {
-    Console.WriteLine("B");
+    Console.WriteLine("onluq mertebe tekliden boyukdur");
 }
-else if (score > 70 && score < 79)
+else if (units > tens)
 {
-    Console.WriteLine("c");
-}
-else if (score > 60 && score < 69)
-{
-    Console.WriteLine("D");
-}
-else if (score > 51 && score < 59)
-{
-    Console.WriteLine("E");
+    Console.WriteLine("teklik onluqdan boyukdur");
 }
 else
 {
-    Console.WriteLine("Bdu telebesidirse kesilir");
+    Console.WriteLine("beraberdirler");
 }
-
-
